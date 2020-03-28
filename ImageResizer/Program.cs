dc81d1b6fad5bca7a60eee41154c231303f07c49
d0972058277 <gn02053231@gmail.com>
2020-03-28 20:58:33 +0800

    tango down

--- ImageResizer/Program.cs
+++ ImageResizer/Program.cs
@@ -3,27 +3,63 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ImageResizer
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            string sourcePath = Path.Combine(Environment.CurrentDirectory, "images");
-            string destinationPath = Path.Combine(Environment.CurrentDirectory, "output"); ;
+            // sync 1.857s
+            // async 1.224s
+            // 34%
+            int round = 10;
 
-            ImageProcess imageProcess = new ImageProcess();
+            //await Task.Run(() =>
+            //{
+            //    long total = 0;
+            //    for (int i = 0; i < round; i++)
+            //    {
+            //        string sourcePath = Path.Combine(Environment.CurrentDirectory, "images");
+            //        string destinationPath = Path.Combine(Environment.CurrentDirectory, "output"); ;
+            //        ImageProcess imageProcess = new ImageProcess();
+            //        imageProcess.Clean(destinationPath);
+            //        Stopwatch sw = new Stopwatch();
+            //        sw.Start();
+            //        imageProcess.ResizeImages(sourcePath, destinationPath, 2.0);
+            //        sw.Stop();
 
-            imageProcess.Clean(destinationPath);
+            //        Console.WriteLine($"同步 花費時間: {sw.ElapsedMilliseconds} ms");
+            //        total += sw.ElapsedMilliseconds;
+            //    }
 
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-            imageProcess.ResizeImages(sourcePath, destinationPath, 2.0);
-            sw.Stop();
+            //    Console.WriteLine($"同步 {total / round}");
+            //});
 
-            Console.WriteLine($"花費時間: {sw.ElapsedMilliseconds} ms");
+            await Task.Run(async () =>
+            {
+                long total = 0;
+                for (int i = 0; i < round; i++)
+                {
+                    string sourcePath = Path.Combine(Environment.CurrentDirectory, "images");
+                    string destinationPath = Path.Combine(Environment.CurrentDirectory, "output"); ;
+                    ImageProcess imageProcess = new ImageProcess();
+                    imageProcess.Clean(destinationPath);
+                    Stopwatch sw = new Stopwatch();
+                    sw.Start();
+                    await imageProcess.ResizeImagesAsync(sourcePath, destinationPath, 2.0);
+                    sw.Stop();
+
+                    Console.WriteLine($"非同步 花費時間: {sw.ElapsedMilliseconds} ms");
+                    total += sw.ElapsedMilliseconds;
+                }
+
+                Console.WriteLine($"非同步 {total / round}");
+            });
+
+            Console.ReadLine();
         }
     }
 }
